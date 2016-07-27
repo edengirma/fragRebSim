068508628cc43cd1a6ea12910645055f8429bcc5
Eden Girma <eden.girma@cfa.harvard.edu>
2016-07-27 11:56:50 -0400

    moved migrationAccel to RebSimIntegrator class in integrator.py

--- fragRebSim/funcs.py
+++ fragRebSim/funcs.py
@@ -1,29 +1,4 @@
-from math import sqrt
-
-from .forces import (bulge_force, cluster_force, disk_force, halo_force,
-                     sim_constants)
-
-
-# Galaxy potential, from http://adsabs.harvard.edu/abs/2014ApJ...793..122K
-# Note: There is a typo in that paper where "a_d" is said to be 2750 kpc,
-# it should be 2.75 kpc.
-def migrationAccel(reb_sim):
-    ps = reb_sim.particles
-
-    x2 = ps[1].x**2
-    y2 = ps[1].y**2
-    z2 = ps[1].z**2
-
-    r = sqrt(x2 + y2 + z2)
-    rho2 = x2 + y2
-    zbd = sqrt(z2 + sim_constants.bd**2)
-
-    ps[1].ax += cluster_force(r, ps[1].x) + bulge_force(r, ps[1].x) +\
-        disk_force(r, ps[1].x, rho2, zbd) + halo_force(r, ps[1].x)
-    ps[1].ay += cluster_force(r, ps[1].x) + bulge_force(r, ps[1].y) +\
-        disk_force(r, ps[1].y, rho2, zbd) + halo_force(r, ps[1].y)
-    ps[1].az += cluster_force(r, ps[1].x) + bulge_force(r, ps[1].z) +\
-        disk_force(r, ps[1].z, rho2, zbd) + halo_force(r, ps[1].z)
+from .forces import sim_constants
 
 
 # Randomly draw stellar mass, derived from Salpeter's mass function:
