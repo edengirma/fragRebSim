4f23bad6fc92911707e7a0caf5332e1a9093ac69
Eden Girma <eden.girma@cfa.harvard.edu>
2016-08-10 15:44:10 -0400

    Edited so that disruptions occur every 1e4 years

--- fragRebSim/otherintegrator.py
+++ fragRebSim/otherintegrator.py
@@ -22,14 +22,19 @@ class RebSimIntegrator:
         self.Nstars = Nstars
         self.Nfrag = Nfrag
         self.Nout = 10000
-        self.max_time = 1.0e7
+        self.max_time = 1.0e7 * 2.0 * np.pi
         self.posx = [[[] for y in range(Nfrag)] for x in range(Nstars)]
         self.posy = [[[] for y in range(Nfrag)] for x in range(Nstars)]
         self.posz = [[[] for y in range(Nfrag)] for x in range(Nstars)]
-        # self.xyz = np.zeros((self.Nstars, self.Nfrag, self.Nout, 3))
-        # self.xyz = [[[] for y in range(Nfrag)] for x in range(Nstars)]
         self.dmde = dMdEdist()
         self.forces = []
+        self.star_masses = []
+        self.star_radii = []
+        self.tidal_radii = []
+
+        # Star-fragment indices list, necessary to keep track of which
+        # particles in the simulation belong to which star.
+        self.sfindices = [0]
 
     def set_Nstars(self, new_Nstars):
         self.Nstars = new_Nstars
@@ -46,162 +51,198 @@ class RebSimIntegrator:
     # 2750 kpc, it should be 2.75 kpc.
     def migrationAccel(self, reb_sim):
         ps = reb_sim.contents.particles
-        x2 = ps[1].x**2
-        y2 = ps[1].y**2
-        z2 = ps[1].z**2
-        r = sqrt(x2 + y2 + z2)
-        rho2 = x2 + y2
-        zbd = sqrt(z2 + sc.bd**2)
 
-        ps[1].ax += cluster_force(r, ps[1].x) + bulge_force(r, ps[1].x) +\
-            disk_force(r, ps[1].x, rho2, zbd) + halo_force(r, ps[1].x)
-        ps[1].ay += cluster_force(r, ps[1].y) + bulge_force(r, ps[1].y) +\
-            disk_force(r, ps[1].y, rho2, zbd) + halo_force(r, ps[1].y)
-        ps[1].az += cluster_force(r, ps[1].z) + bulge_force(r, ps[1].z) +\
-            disk_force(r, ps[1].z, rho2, zbd) + halo_force(r, ps[1].z)
+        # Apply forces to every fragment
+        for p in ps[1:]:
+            x2 = p.x**2
+            y2 = p.y**2
+            z2 = p.z**2
+            r = sqrt(x2 + y2 + z2)
+            rho2 = x2 + y2
+            zbd = sqrt(z2 + sc.bd**2)
 
-    def sim_integrate(self):
+            p.ax += cluster_force(r, p.x) + bulge_force(r, p.x) +\
+                disk_force(r, p.x, rho2, zbd) + halo_force(r, p.x)
+            p.ay += cluster_force(r, p.y) + bulge_force(r, p.y) +\
+                disk_force(r, p.y, rho2, zbd) + halo_force(r, p.y)
+            p.az += cluster_force(r, p.z) + bulge_force(r, p.z) +\
+                disk_force(r, p.z, rho2, zbd) + halo_force(r, p.z)
+
+    # Star disruption function
+    def star_disrupt(self, reb_sim):
         m_hole = sc.m_hole
-        star_masses = []
-        star_radii = []
-        tidal_radii = []
 
-        for star in tnrange(self.Nstars, desc='Star', leave=False):
-            # Randomly drawn mass of star
-            xstar = rnd.random()
-            m_star = mstar_dist(xstar)
-            star_masses.append(m_star)
+        # Randomly drawn mass of star
+        xstar = rnd.random()
+        m_star = mstar_dist(xstar)
+        self.star_masses.append(m_star)
 
-            # Determined radius of star from stellar mass
-            r_star = rstar_func(m_star) * sc.RsuntoAU
-            star_radii.append(r_star)
+        # Determined radius of star from stellar mass
+        r_star = rstar_func(m_star) * sc.RsuntoAU
+        self.star_radii.append(r_star)
 
-            # Distance spread for fragments
-            rads = [r_star * float(f) / float(self.Nfrag + 1)
-                    for f in range(self.Nfrag + 1)]
-            rads.pop(0)
+        # Distance spread for fragments
+        rads = [r_star * float(f) / float(self.Nfrag + 1)
+                for f in range(self.Nfrag + 1)]
+        rads.pop(0)
 
-            # Determined tidal radius of star
-            r_t = r_tidal(m_star, r_star)
-            tidal_radii.append(r_t)
+        # Determined tidal radius of star
+        r_t = r_tidal(m_star, r_star)
+        self.tidal_radii.append(r_t)
 
-            # Set position of star; random sphere point picking
-            u1 = rnd.uniform(-1.0, 1.0)
-            th1 = rnd.uniform(0., 2. * np.pi)
-            star_direc = np.array([sqrt(1.0 - (u1)**2) * cos(th1),
-                                   sqrt(1.0 - (u1)**2) * sin(th1),
-                                   u1])
-            star_vec = [r_t * d for d in star_direc]
+        # Set position of star; random sphere point picking
+        u1 = rnd.uniform(-1.0, 1.0)
+        th1 = rnd.uniform(0., 2. * np.pi)
+        star_direc = np.array([sqrt(1.0 - (u1)**2) * cos(th1),
+                               sqrt(1.0 - (u1)**2) * sin(th1),
+                               u1])
+        star_vec = [r_t * d for d in star_direc]
 
-            # Binding energy spread, with beta value randomly drawn from
-            # beta distribution
-            xbeta = rnd.random()
-            beta = beta_dist(xbeta)
-            NRGs = self.dmde.energy_spread(beta, self.Nfrag)
+        # Binding energy spread, with beta value randomly drawn from
+        # beta distribution
+        xbeta = rnd.random()
+        beta = beta_dist(xbeta)
+        NRGs = self.dmde.energy_spread(beta, self.Nfrag)
 
-            # Converted NRGs list from cgs to proper units
-            pi = np.pi
-            natural_u = (u.AU / (u.yr / (2.0 * pi)))**2
-            nrg_scale = ((r_star * sc.AUtoRsun)**(-1.0) * (m_star)**(2.0 / 3.0)
-                         * (m_hole / 1.0e6)**(1.0 / 3.0))
-            energies = [(nrg_scale * nrg *
-                         (u.cm / u.second)**2).to(natural_u).value
-                        for nrg in NRGs]
+        # Converted NRGs list from cgs to proper units
+        pi = np.pi
+        natural_u = (u.AU / (u.yr / (2.0 * pi)))**2
+        nrg_scale = ((r_star * sc.AUtoRsun)**(-1.0) * (m_star)**(2.0 / 3.0)
+                     * (m_hole / 1.0e6)**(1.0 / 3.0))
+        energies = [(nrg_scale * nrg *
+                     (u.cm / u.second)**2).to(natural_u).value
+                    for nrg in NRGs]
 
-            # Calculating velocities
-            vels = [sqrt((2.0 * g) + (2 * m_hole / r_t)) for g in energies]
+        # Calculating velocities
+        vels = [sqrt((2.0 * g) + (2 * m_hole / r_t)) for g in energies]
 
-            # Randomly draw velocity vector direction
-            phi2 = rnd.uniform(0., 2. * np.pi)
+        # Randomly draw velocity vector direction
+        phi2 = rnd.uniform(0., 2. * np.pi)
 
-            x = star_vec[0]
-            y = star_vec[1]
-            z = star_vec[2]
-            r = np.linalg.norm(star_vec)
+        x = star_vec[0]
+        y = star_vec[1]
+        z = star_vec[2]
+        r = np.linalg.norm(star_vec)
 
-            randomvelvec = [
-                (x * (r - z + z * cos(phi2)) - r * y * sin(phi2)) /
-                (r**2 * sqrt(2.0 - 2.0 * z / r)),
-                (y * (r - z + z * cos(phi2)) + r * x * sin(phi2)) /
-                (r**2 * sqrt(2.0 - 2.0 * z / r)),
-                ((r - z) * z - (x**2 + y**2) * cos(phi2)) /
-                (r**2 * sqrt(2.0 - 2.0 * z / r))
-            ]
+        randomvelvec = [
+            (x * (r - z + z * cos(phi2)) - r * y * sin(phi2)) /
+            (r**2 * sqrt(2.0 - 2.0 * z / r)),
+            (y * (r - z + z * cos(phi2)) + r * x * sin(phi2)) /
+            (r**2 * sqrt(2.0 - 2.0 * z / r)),
+            ((r - z) * z - (x**2 + y**2) * cos(phi2)) /
+            (r**2 * sqrt(2.0 - 2.0 * z / r))
+        ]
 
-            velocity_vec = np.cross(star_vec, randomvelvec)
-            n = np.linalg.norm(velocity_vec)
-            vel_direc = [v / n for v in velocity_vec]
+        velocity_vec = np.cross(star_vec, randomvelvec)
+        n = np.linalg.norm(velocity_vec)
+        vel_direc = [v / n for v in velocity_vec]
 
-            # Set up rebound simulation
-            reb_sim = rebound.Simulation()
-            reb_sim.integrator = "ias15"
-            reb_sim.add(m=m_hole)
-            reb_sim.dt = 1.0e-15
+        for frag in tnrange(self.Nfrag, desc='Fragment', leave=False):
 
-            for frag in tnrange(self.Nfrag, desc='Fragment', leave=False):
+            # Velocity vector of fragment
+            vel = vels[frag]
+            frag_velvec = [vel * v for v in vel_direc]
 
-                # Velocity vector of fragment
-                vel = vels[frag]
-                frag_velvec = [vel * v for v in vel_direc]
+            # Position vector of Fragment
+            rad = rads[frag]
+            frag_posvec = [(r_t + rad) * p for p in star_direc]
 
-                # Position vector of Fragment
-                rad = rads[frag]
-                frag_posvec = [(r_t + rad) * p for p in star_direc]
+            # Add particle to rebound simulation
+            reb_sim.add(m=0.0, x=frag_posvec[0], y=frag_posvec[1],
+                        z=frag_posvec[2], vx=frag_velvec[0],
+                        vy=frag_velvec[1], vz=frag_velvec[2])
+            self.sfindices.append(reb_sim.N - 1)
 
-                # Add particle to rebound simulation
-                reb_sim.add(m=0.0, x=frag_posvec[0], y=frag_posvec[1],
-                            z=frag_posvec[2], vx=frag_velvec[0],
-                            vy=frag_velvec[1], vz=frag_velvec[2])
+    # Removing fragment from posx, posy, and posz arrays given its index
+    # in the array of reb_sim particles
+    def remove_fragment_record(self, particle_index):
+        pi = particle_index
+        for i, stop_index in enumerate(self.sfindices):
+            if stop_index > pi or stop_index == pi:
+                start = self.sfindices[i-1] + 1
+                new_pi = pi - start
+                star = i-1
+                self.posx[star].pop(new_pi)
+                self.posy[star].pop(new_pi)
+                self.posz[star].pop(new_pi)
 
-            reb_sim.N_active = 1
-            reb_sim.additional_forces = self.migrationAccel
-            reb_sim.force_is_velocity_dependent = 1
-            reb_sim.exit_max_distance = 15.0 * sc.scale  # 15 pc in AU
-            escaped_particles = 0
-            removed_particles = 0
+                for j, index in enumerate(self.sfindices[i:]):
+                    self.sfindices[j+i] -= 1
+                return
+            else:
+                continue
 
-            stop = np.log10(self.max_time)
-            times = np.logspace(-17.0, stop, self.Nout - 1)
-            times = np.insert(times, 0.0, 0)
-            for ti, time in enumerate(tqdm(times)):
-                try:
-                    reb_sim.integrate(time, exact_finish_time=1)
+    def record_fragment_positions(self, reb_sim):
+        i = 1
+        stop_index = self.sfindices[i]
+        while i < self.Nstars + 1:
+            for pi, p in enumerate(reb_sim.particles):
+                if pi == 0:
+                    continue
+                frag = pi - self.sfindices[i-1] - 1
+                star = i - 1
+                self.posx[star][frag].append(p.x / sc.scale)
+                self.posy[star][frag].append(p.y / sc.scale)
+                self.posz[star][frag].append(p.z / sc.scale)
+                if pi == stop_index and i + 1 == self.Nstars + 1:
+                    return
+                elif pi == stop_index:
+                    i += 1
+                    stop_index = self.sfindices[i]
 
-                    # Semi-major axis criterion:
-                    # Cuts particles closely bound to black hole
-                    for pi, p in enumerate(reb_sim.particles[1:]):
-                        try:
-                            if (pi != 0 and
-                                    2.0 * p.a / sc.scale > 0.0 and
-                                    2.0 * p.a / sc.scale < 1.0):
-                                reb_sim.remove(pi)
-                                self.posx[star].pop(pi-1)
-                                self.posy[star].pop(pi-1)
-                                self.posz[star].pop(pi-1)
-                                removed_particles += 1
-                        except IndexError:
-                            print('index: ', pi)
-                            print('number of active particles: ', reb_sim.N)
-                except rebound.Escape:  # Removes escaped particles
-                    for j in range(reb_sim.N):
-                        p = reb_sim.particles[j]
-                        d2 = p.x * p.x + p.y * p.y + p.z * p.z
-                        if d2 > reb_sim.exit_max_distance**2:
-                            index = j
-                            reb_sim.remove(index=index)
-                            self.posx[star].pop(j-1)
-                            self.posy[star].pop(j-1)
-                            self.posz[star].pop(j-1)
-                            escaped_particles += 1
+    # Integrating simulation
+    def sim_integrate(self):
+        # Set up rebound simulation
+        reb_sim = rebound.Simulation()
+        reb_sim.integrator = "ias15"
+        reb_sim.add(m=sc.m_hole)
+        reb_sim.dt = 1.0e-15
 
-                # Recording positions of fragments
-                for pi, p in enumerate(reb_sim.particles[1:]):
-                    if pi == 0:
-                        continue  # Ignore position of BH
-                    self.posx[star][pi - 1].append(p.x / sc.scale)
-                    self.posy[star][pi - 1].append(p.y / sc.scale)
-                    self.posz[star][pi - 1].append(p.z / sc.scale)
-            print('Escaped particles: ', escaped_particles)
-            print('Removed particles: ', removed_particles)
+        reb_sim.N_active = 1
+        reb_sim.additional_forces = self.migrationAccel
+        reb_sim.force_is_velocity_dependent = 1
+        reb_sim.exit_max_distance = 15.0 * sc.scale  # 15 pc in AU
 
-        print('Masses:', star_masses)
+        stop = np.log10(self.max_time)
+        times = np.logspace(-17.0, stop, self.Nout - 1)
+        times = np.insert(times, 0.0, 0)
+        epsilon = 0.1 * (1.0e4 * 2 * np.pi)
+
+        # Disrupt first star
+        self.star_disrupt(reb_sim)
+        star_count = 1
+        # Begin integration
+        for ti, time in enumerate(tqdm(times)):
+            try:
+                reb_sim.integrate(time, exact_finish_time=1)
+
+                # Add new disruption every 10,000 years
+                while star_count != self.Nstars:
+                    if time % (1.0e4 * 2 * np.pi) < epsilon:
+                        self.star_disrupt(reb_sim)
+                        star_count += 1
+                        print('Star disrupted')
+
+                # Semi-major axis criterion:
+                # Cuts particles closely bound to black hole
+                for pi, p in enumerate(reb_sim.particles):
+                    try:
+                        if (pi != 0 and
+                                2.0 * p.a / sc.scale > 0.0 and
+                                2.0 * p.a / sc.scale < 2.0):
+                            reb_sim.remove(pi)
+                            self.remove_fragment_record(pi)
+                    except IndexError:
+                        print('index: ', pi)
+                        print('number of active particles: ', reb_sim.N)
+            except rebound.Escape:  # Removes escaped particles
+                for j in range(reb_sim.N):
+                    p = reb_sim.particles[j]
+                    d2 = p.x * p.x + p.y * p.y + p.z * p.z
+                    if d2 > reb_sim.exit_max_distance**2:
+                        index = j
+                        reb_sim.remove(index)
+                        self.remove_fragment_record(index)
+
+            # Recording positions of fragments
+            self.record_fragment_positions(reb_sim)
